7a5349810ad6241f6ccc30900c9e3e85873c9df7
Antonio Vilchez <antoniovmonge@gmail.com>
2023-11-16 09:27:50 +0100

    Update requirements.txt and use environment variable for JWT secret key

--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,11 +1,16 @@
+import os
+
 from jwt import encode, decode
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_token(data: dict) -> str:
-    token: str = encode(payload=data, key="secret_1234", algorithm="HS256")
+    token: str = encode(payload=data, key=os.getenv("SECRET_KEY"), algorithm="HS256")
     return token
 
 
 def validate_token(token: str) -> dict:
-    data: dict = decode(token, key="secret_1234", algorithms=["HS256"])
+    data: dict = decode(token, key=os.getenv("SECRET_KEY"), algorithms=["HS256"])
     return data
